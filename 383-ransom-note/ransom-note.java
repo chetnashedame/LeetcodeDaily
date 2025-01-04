d71c3da848ced58f7effa74c7a10f9d30708e97d
chetnashedame <71824668+chetnashedame@users.noreply.github.com>
2025-01-05 03:24:47 +0530

    Time: 1 ms (99.49%) | Memory: 44.7 MB (81.29%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -3,13 +3,13 @@ class Solution {
 
         int [] arr = new int[26];
         for(char c: ransomNote.toCharArray()){
-            arr[c-'a']++;
+            arr[c-'a']++;   //maps the each char c to its index e.g 'a'>0, 'b'>1 and increment the count of each letter
         }
         for(char c: magazine.toCharArray()){
             arr[c-'a']--;
         }
         for(int i:arr){
-            if(i>0)
+            if(i>0)   //if count is positive it means ransomnote needs more letters than magazines provide
             return false;
         }
         return true;
